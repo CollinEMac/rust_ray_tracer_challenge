8149136f9bb0258807013690ec289ed7bd09b3a1
= <macd.collin@gmail.com>
2022-10-25 20:34:55 -0500

    Create matrices (kind of)

--- src/main.rs
+++ src/main.rs
@@ -556,4 +556,21 @@ mod tests {
         }
     }
 
+    #[test]
+    fn test_constructing_and_inspecting_a_4x4_matrix() {
+        let matrix = vec![
+            vec![1.0, 2.0, 3.0, 4.0],
+            vec![5.5, 6.5, 7.5, 8.5],
+            vec![9.0, 10.0, 11.0, 12.0],
+            vec![13.5, 14.5, 15.5, 16.5]
+        ];
+
+        assert_eq!(matrix[0][0], 1.0);
+        assert_eq!(matrix[0][3], 4.0);
+        assert_eq!(matrix[1][0], 5.5);
+        assert_eq!(matrix[1][2], 7.5);
+        assert_eq!(matrix[2][2], 11.0);
+        assert_eq!(matrix[3][0], 13.5);
+        assert_eq!(matrix[3][2], 15.5);
+    }
 }
